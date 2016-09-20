a95ea12b2921891ee03f889217359ba0070d1c05
weatherfish <weatherfish2010@gmail.com>
2016-09-21 01:16:15 +0800

    gg

---
+++ demo/src/main/java/com/liulishuo/filedownloader/demo/MyView.java
@@ -0,0 +1,20 @@
+package com.liulishuo.filedownloader.demo;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by weatherfish on 16/3/7.
+ */
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
